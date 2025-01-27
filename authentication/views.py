7a385b90846bb3b9c30536cb32f717426a505360
dariusfanou <dariusfanou@gmail.com>
2025-01-27 20:33:55 +0100

    Update users

--- authentication/views.py
+++ authentication/views.py
@@ -8,18 +8,16 @@ class UserViewSet(ModelViewSet):
 
     serializer_class = UserSerializer
 
-    # def get_permissions(self):
+    def get_permissions(self):
 
-    #     if self.action == 'create':
-    #         permission_classes = [AllowAny]
-    #     else:
-    #         permission_classes = [IsAuthenticated]
+        if self.action == 'create':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
 
-    #     return [permission() for permission in permission_classes]
+        return [permission() for permission in permission_classes]
 
     def get_queryset(self):
-        # if self.request.user.is_superuser:  # Si l'utilisateur est admin, il voit tout
-        #     return User.objects.all()
-        # return User.objects.filter(id=self.request.user.id)  # Sinon, uniquement ses propres données
-        return User.objects.all()
-
+        if self.request.user.is_superuser:  # Si l'utilisateur est admin, il voit tout
+            return User.objects.all()
+        return User.objects.filter(id=self.request.user.id)  # Sinon, uniquement ses propres données
